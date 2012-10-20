ca8a2b1f1345c6e1a13f1cd5f47ceabed32e0e5d
Sung Choi <m.sung.choi@gmail.com>
2012-10-20 18:15:28 +0900

    creates method to pass tests, fixes a bug in the tests

--- triangle?/triangle_spec.rb
+++ triangle?/triangle_spec.rb
@@ -1,14 +1,16 @@
 require_relative './triangle.rb'
 require 'rspec'
+include Triangle
 
 describe '#triangle?' do
   context "given three sides" do
     it "returns true if it is a real triangle" do
+      triangle?(1,2,2).should eq true
       triangle?(3,4,5).should eq true
       triangle?(2,2,3).should eq true
     end
     it "returns false if it cannot be a triangle" do
-      triangle?(1,1,1).should eq false
+      triangle?(1,1,2).should eq false
       triangle?(1,1,100).should eq false
     end
   end
