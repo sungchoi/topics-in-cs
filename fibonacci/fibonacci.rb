a91d06fc9103a3976c8eb58b5434fe924e19c6da
Sung Choi <m.sung.choi@gmail.com>
2012-12-08 15:45:01 -0800

    new iteration on fibonacci methods

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -13,16 +13,39 @@ def fibonacci_recursive_with_dp(index) # dynamic processing stores the result of
   if fibonacci_sequence[index]
     fibonacci_sequence[index]
   else
-    fibonacci_recursive_with_dp(index - 1) + fibonacci_recursive_with_dp(index - 2)
+    fibonacci_sequence[index] = fibonacci_recursive_with_dp(index - 1) + fibonacci_recursive_with_dp(index - 2)
   end
 end
 
-def fibonacci_recursive_with_dp_enhanced(index) 
+def fibonacci_nonrecursive(index) 
 # dynamic processing stores the result of each recursive calculation so that redundant calculations are not carried out
 # only the last two numbers calculated for the sequence are stored at any one time
-
+  fibonacci_sequence = [0,1]
+  if fibonacci_sequence[index]
+    fibonacci_sequence[index]
+  else
+    (index - 1).times do
+      temp = fibonacci_sequence[0] + fibonacci_sequence[1] # save next number in sequence
+      fibonacci_sequence[0] = fibonacci_sequence[1] # move the number in index 1 to index 0
+      fibonacci_sequence[1] = temp # store next number in the sequence in index 1
+    end
+    fibonacci_sequence[1] # return last number
+  end
 end
 
-def fibonacci_nonrecursive(index)
-
+def fibonacci_nonrecursive_old(index)
+  fibonacci_sequence = [0,1]
+  if fibonacci_sequence[index]
+    fibonacci_sequence[index]
+  else 
+    (index / 2).times do
+      fibonacci_sequence[0] = fibonacci_sequence[0] + fibonacci_sequence[1]
+      fibonacci_sequence[1] = fibonacci_sequence[0] + fibonacci_sequence[1]
+    end
+    if index % 2 == 0 # if index is even
+      fibonacci_sequence[0]
+    else
+      fibonacci_sequence[1]
+    end
+  end
 end
--- fibonacci/fibonacci_spec.rb
+++ fibonacci/fibonacci_spec.rb
@@ -19,20 +19,20 @@ describe "fibonacci_recursive_with_dp" do
   end
 end
 
-describe "fibonacci_recursive_with_dp_enhanced" do
-  it "returns the correct number from the fibonacci sequence given an index number starting with zero" do
-    fibonacci_recursive_with_dp_enhanced(0).should eq 0
-    fibonacci_recursive_with_dp_enhanced(1).should eq 1
-    fibonacci_recursive_with_dp_enhanced(2).should eq 1
-    fibonacci_recursive_with_dp_enhanced(10).should eq 55    
-  end
-end
-
 describe "fibonacci_nonrecursive" do
   it "returns the correct number from the fibonacci sequence given an index number starting with zero" do
     fibonacci_nonrecursive(0).should eq 0
     fibonacci_nonrecursive(1).should eq 1
     fibonacci_nonrecursive(2).should eq 1
     fibonacci_nonrecursive(10).should eq 55    
+  end
+end
+
+describe "fibonacci_nonrecursive_old" do
+  it "returns the correct number from the fibonacci sequence given an index number starting with zero" do
+    fibonacci_nonrecursive_old(0).should eq 0
+    fibonacci_nonrecursive_old(1).should eq 1
+    fibonacci_nonrecursive_old(2).should eq 1
+    fibonacci_nonrecursive_old(10).should eq 55    
   end
 end
