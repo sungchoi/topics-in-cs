ede0f48d12b391a9d4e6bd32e116cdd59baa5355
Sung Choi <m.sung.choi@gmail.com>
2012-12-31 11:01:10 -0800

    fixes specs and makes minor refactor on factorial_loop

--- factorial/factorial.rb
+++ factorial/factorial.rb
@@ -1,11 +1,11 @@
+def factorial_inject(num)
+  (1..num).inject{|product, x| product * x}
+end
+
 def factorial_loop(num)
   product = 1
   1.upto(num) do |x|
-    product = product * x
+    product *= x
   end
   product
-end
-
-def factorial_inject(num)
-  (1..num).inject{|product, x| product * x}
 end
--- factorial/factorial_spec.rb
+++ factorial/factorial_spec.rb
@@ -3,14 +3,14 @@ require 'rspec'
 
 describe '#factorial_loop' do
   it ' ' do
-    factorial(3).should eq (3 * 2 * 1)
-    factorial(10).should eq (10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1)
+    factorial_loop(3).should eq (3 * 2 * 1)
+    factorial_loop(10).should eq (10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1)
   end
 end
 
 describe '#factorial_inject' do
   it ' ' do
-    factorial2(3).should eq (3 * 2 * 1)
-    factorial2(10).should eq (10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1)
+    factorial_inject(3).should eq (3 * 2 * 1)
+    factorial_inject(10).should eq (10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1)
   end
 end
