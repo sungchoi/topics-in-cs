c225e650a19f4c2ff0f455c9defbefe4bfbf15ea
Sung Choi <m.sung.choi@gmail.com>
2015-01-27 17:48:52 -0500

    SymbolicCalculator. Separates out calculator tests from expression_spec.rb into its own file.

--- symbolic-calculator/draft-two/spec/expression_spec.rb
+++ symbolic-calculator/draft-two/spec/expression_spec.rb
@@ -1,23 +1,6 @@
 require 'rspec'
 
 require_relative '../expression.rb'
-require_relative '../symbolic_calculator.rb'
-
-describe SymbolicCalculator::Calculator do
-  context "#evaluate" do
-    it "calculates" do 
-      expect(SymbolicCalculator::Calculator.new("2 2 *").evaluate.to_s).to eq("4")
-      expect(SymbolicCalculator::Calculator.new("2 3 +").evaluate.to_s).to eq("5")
-      expect(SymbolicCalculator::Calculator.new("x x *").evaluate.to_s).to eq("x 2 **")
-      expect(SymbolicCalculator::Calculator.new("x x * x *").evaluate.to_s).to eq("x 3 **")
-      expect(SymbolicCalculator::Calculator.new("x x * x /").evaluate.to_s).to eq("x 1 **")
-      expect(SymbolicCalculator::Calculator.new("x 0 *").evaluate.to_s).to eq("0")
-      expect(SymbolicCalculator::Calculator.new("x 0 /").evaluate.to_s).to eq("Infinity")
-      expect(SymbolicCalculator::Calculator.new("2 2 + x 0 / +").evaluate.to_s).to eq("Infinity")
-
-    end
-  end
-end
 
 describe SymbolicCalculator::Expression do
   context "#evaluate" do
