c3535e60ece1711498de6f9087e3f2ed477bd57a
Sung Choi <m.sung.choi@gmail.com>
2015-01-27 17:50:34 -0500

    SymbolicCalculator. Separates out ARITHMETIC_TABLE tests into its own file.

--- symbolic-calculator/draft-two/spec/expression_spec.rb
+++ symbolic-calculator/draft-two/spec/expression_spec.rb
@@ -52,37 +52,3 @@ describe SymbolicCalculator::Expression do
     end
   end
 end
-
-describe SymbolicCalculator::ARITHMETIC_TABLE do
-  it "does addition" do
-    left  = Random.new.rand(100)
-    right = Random.new.rand(100)
-    total = left + right
-    expect(SymbolicCalculator::ARITHMETIC_TABLE["+"].call(left, right)).to eq(total)
-    expect(SymbolicCalculator::ARITHMETIC_TABLE["+"].call(2, 2)).to eq(4)
-  end
-
-  it "does subtraction" do
-    left  = Random.new.rand(100)
-    right = Random.new.rand(100)
-    total = left - right
-    expect(SymbolicCalculator::ARITHMETIC_TABLE["-"].call(left, right)).to eq(total)
-    expect(SymbolicCalculator::ARITHMETIC_TABLE["-"].call(2, 2)).to eq(0)
-  end
-
-  it "does multiplication" do
-    left  = Random.new.rand(100)
-    right = Random.new.rand(100)
-    total = left * right
-    expect(SymbolicCalculator::ARITHMETIC_TABLE["*"].call(left, right)).to eq(total)
-    expect(SymbolicCalculator::ARITHMETIC_TABLE["*"].call(2, 2)).to eq(4)
-  end
-
-  it "does division" do
-    left  = Random.new.rand(100)
-    right = Random.new.rand(100)
-    total = left / right
-    expect(SymbolicCalculator::ARITHMETIC_TABLE["/"].call(left, right)).to eq(total)
-    expect(SymbolicCalculator::ARITHMETIC_TABLE["/"].call(4, 2)).to eq(2)
-  end
-end
