16a2edbc1e4693d72d27c9710d18d801f9c5efe1
Sung Choi <m.sung.choi@gmail.com>
2015-03-27 09:48:09 -0700

    combinations_of_money. Removes log messages.

--- combinations_of_money.rb
+++ combinations_of_money.rb
@@ -8,7 +8,6 @@ def combinations_of_money(amount, denominations = [0.01, 0.05, 0.1, 0.25, 0.5, 1
   amount = amount.to_d
   denominations = denominations.sort.map { |e| e.to_d }
   quotient = amount / denominations[-1]
-  puts "amount: #{amount}. denominations: #{denominations}. memo: #{memo}. quotient: #{quotient}"
 
   if denominations.length == 0
     return memo
@@ -19,12 +18,8 @@ def combinations_of_money(amount, denominations = [0.01, 0.05, 0.1, 0.25, 0.5, 1
     denom = denominations[-1]
     denominations = denominations[0..-2]
     0.upto(quotient) do |i|
-      puts "i: #{i}. denom: #{denom}. denominations: #{denominations}. memo: #{memo}"
-
       combinations_of_money(amount - i * denom, denominations).each do |combination|
-          puts "combination: #{combination}"
           memo << {denom => i}.merge(combination)
-          puts "memo: #{memo}"
       end
     end
   end
