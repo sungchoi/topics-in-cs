2ea84660be70b0d44af1fb3d0a60a84e0a01b61d
Sung Choi <m.sung.choi@gmail.com>
2015-03-27 09:31:56 -0700

    combinations_of_money. Renames function name from camelcase to train case. Fixes bug so that an empty array is returned when no combinations are possible. Normalizes all amounts and denominations using BigDecimal in order to prevent floating point issues. Removes log messages.

--- combinations_of_money.rb
+++ combinations_of_money.rb
@@ -1,24 +1,27 @@
 # 1. Write a function that takes in an amount of money and denominations and returns the number of ways to arrive at the amount with those denominations.
 #order does not matter
-def combinationsOfMoney(amount, denominations = [0.01, 0.05, 0.1, 0.25, 0.5, 1.0, 1, 2, 5, 10, 20, 50, 100], memo = [])
-  denominations = denominations.sort
+
+require 'bigdecimal'
+require 'bigdecimal/util'
+
+def combinations_of_money(amount, denominations = [0.01, 0.05, 0.1, 0.25, 0.5, 1.0, 1, 2, 5, 10, 20, 50, 100], memo = [])
+  amount = amount.to_d
+  denominations = denominations.sort.map { |e| e.to_d }
   quotient = amount / denominations[-1]
   puts "amount: #{amount}. denominations: #{denominations}. memo: #{memo}. quotient: #{quotient}"
 
   if denominations.length == 0
     return memo
   elsif denominations.length == 1
-    # memo if quotient == 0
+    return memo if amount % denominations[-1] != 0
     memo.push({denominations[-1] => quotient})
   else
-    denom = denominations.pop
+    denom = denominations[-1]
+    denominations = denominations[0..-2]
     0.upto(quotient) do |i|
-      puts "i: #{i}"
-      puts "denom: #{denom}"
-      puts "denominations: #{denominations}"
-      puts "memo: #{memo}"
+      puts "i: #{i}. denom: #{denom}. denominations: #{denominations}. memo: #{memo}"
 
-      combinationsOfMoney(amount - i * denom, denominations).each do |combination|
+      combinations_of_money(amount - i * denom, denominations).each do |combination|
           puts "combination: #{combination}"
           memo << {denom => i}.merge(combination)
           puts "memo: #{memo}"
@@ -32,25 +35,24 @@ def combinationsOfMoney(amount, denominations = [0.01, 0.05, 0.1, 0.25, 0.5, 1.0
   end
 end
 
-# replace denominations.pop with a non destructive assignment
-# floating point issue
-
 require 'rspec'
+require 'bigdecimal'
+require 'bigdecimal/util'
 
-RSpec.describe "combinationsOfMoney" do
+RSpec.describe "combinations_of_money" do
   it "" do
-    expect(combinationsOfMoney(100, [100])).to eq([{100 => 1}])
+    expect(combinations_of_money(100, [100])).to eq([{100.to_d => 1.to_d}])
   end
 
   it "" do
-    expect(combinationsOfMoney(99, [100])).to eq([])
+    expect(combinations_of_money(99, [100])).to eq([])
   end
 
   it "" do
-    expect(combinationsOfMoney(100, [1, 100])).to eq([{100 => 0, 1 => 100}, {100 => 1, 1 => 0}])
+    expect(combinations_of_money(100, [1, 100])).to eq([{100.to_d => 0.to_d, 1.to_d => 100.to_d}, {100.to_d => 1.to_d, 1.to_d => 0.to_d}])
   end
 
-    it "" do
-    expect(combinationsOfMoney(99, [1, 100])).to eq([{100 => 0, 1 => 99}])
+  it "" do
+    expect(combinations_of_money(99, [1, 100])).to eq([{100.to_d => 0.to_d, 1.to_d => 99.to_d}])
   end
 end
