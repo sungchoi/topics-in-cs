c3bf5babb520956e8d91a76d1c90dd205d21adad
Sung Choi <m.sung.choi@gmail.com>
2015-04-09 20:52:14 -0700

    Update. Functions. Ruby. Updates greatest_sum_contiguous_subarray_ brute_force to return the range indicated the greatest number when the array given contains only negative integers.

--- functions/ruby/greatest_sum_contiguous_subarray_brute_force.rb
+++ functions/ruby/greatest_sum_contiguous_subarray_brute_force.rb
@@ -1,8 +1,15 @@
 def greatest_sum_contiguous_subarray_brute_force(array)
   max_sum = 0
   max_range = (0...0)
+  greatest_number_index = 0
+  greatest_number = array[0]
 
   array.each_with_index do |int, index|
+    if int > greatest_number
+      greatest_number = int
+      greatest_number_index = index
+    end
+
     1.upto(array.length - index) do |i|
       sum = array[index...(index + i)].reduce(&:+)
       if sum > max_sum
@@ -12,5 +19,9 @@ def greatest_sum_contiguous_subarray_brute_force(array)
     end
   end
 
-  max_range
+  if (!array.empty? && max_range.last == max_range.first)
+    (greatest_number_index...(greatest_number_index + 1))
+  else
+    max_range
+  end
 end
