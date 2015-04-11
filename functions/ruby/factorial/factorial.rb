9ff5bb338578a06fadcc1dc5c6c8d5e48897cfa9
Sung Choi <m.sung.choi@gmail.com>
2015-04-10 21:56:09 -0700

    Update. Functions. Ruby. Adds a plan factorial(n) function to the factorial.rb file. Updates the functions to use the variable name n instead of num and i instead of x.

--- functions/ruby/factorial/factorial.rb
+++ functions/ruby/factorial/factorial.rb
@@ -1,11 +1,16 @@
-def factorial_inject(num)
-  (1..num).inject{|product, x| product * x}
+def factorial(n)
+  (1..n).inject { |product, i| product * i }
 end
 
-def factorial_loop(num)
+def factorial_inject(n)
+  (1..n).inject{ |product, i| product * i }
+end
+
+def factorial_loop(n)
   product = 1
-  1.upto(num) do |x|
-    product *= x
+  1.upto(n) do |i|
+    product *= i
   end
+
   product
 end
