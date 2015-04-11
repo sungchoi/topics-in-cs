857bbb4b6e4b97c87fda3e1d9910709c9f34a7a9
Sung Choi <m.sung.choi@gmail.com>
2015-04-10 23:42:51 -0700

    New. Functions Ruby. Adds a pending test in a new shuffle_spec file for shuffle function. I'm not sure that a test is the best way to prove that this function does in fact produce results where each permutation has a 1/n! chance of being produced. It's probably better to show mathematically: (1/n)(1/(n-1))...(1/1) = 1/n!.

---
+++ functions/ruby/spec/shuffle_spec.rb
@@ -0,0 +1,7 @@
+require_relative "../shuffle!"
+
+require "rspec"
+
+describe "shuffle!" do
+  it "should shuffle an array so that each permutation has 1 / factorial(array.length) chance of being produced"
+end
