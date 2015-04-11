14a6e90c7e6d74b4d30d778562eb5130b519d4ec
Sung Choi <m.sung.choi@gmail.com>
2015-04-10 21:27:03 -0700

    Update. Functions. Ruby. Updates number_of_paths_in_2d_array_to(x, y) to use x and y as the parameter names. Updates the function to correspond with the new x and y coordinate convention. Updates the specs and adds additional specs.

--- functions/ruby/number_of_paths_in_a_2d_array_to.rb
+++ functions/ruby/number_of_paths_in_a_2d_array_to.rb
@@ -1,18 +1,15 @@
-def number_of_paths_in_a_2d_array_to(m, n)
-  return 1 if m == 0 && n == 0
-  two_d_array = [[0] + Array.new(n, 1), [1] + Array.new(n, 0)]
+def number_of_paths_in_a_2d_array_to(x, y)
+  return 1 if y == 0 && x == 0
+  two_d_array = [[0] + Array.new(x, 1), [1] + Array.new(x, 0)]
 
-  (1..m).each do |row|
-    (1..n).each do |col_index|
+  (1..y).each do |row|
+    (1..x).each do |col_index|
       two_d_array[1][col_index] = two_d_array[1][col_index - 1] + two_d_array[0][col_index]
     end
 
     two_d_array[0] = two_d_array[1]
-    two_d_array[1] = [1] + Array.new(n, 0)
+    two_d_array[1] = [1] + Array.new(x, 0)
   end
 
-  two_d_array[0][n]
+  two_d_array[0][x]
 end
-
-
-
--- functions/ruby/spec/number_of_paths_in_a_2d_array_to_spec.rb
+++ functions/ruby/spec/number_of_paths_in_a_2d_array_to_spec.rb
@@ -3,14 +3,24 @@ require_relative "../number_of_paths_in_a_2d_array_to"
 require "rspec"
 
 describe "number_of_paths_in_a_2d_array_to" do
-  it "should return 0 given a 2D array of size 0,0" do
+  it "should return 1 given the coordinates (0, 0)" do
     expect(number_of_paths_in_a_2d_array_to(0,0)).to eq(1)
   end
 
-  it "should return the correct number of paths from the root index(0, 0) of the 2D array to the index(m, n)" do
+  it "should return 1 given any coordinates of the form (0, y) or (x, 0)" do
+    expect(number_of_paths_in_a_2d_array_to(0, 1)).to eq(1)
+    expect(number_of_paths_in_a_2d_array_to(2, 0)).to eq(1)
+  end
+
+  it "should return the correct number of paths to coordinates(x, y) from the root (0, 0)" do
     expect(number_of_paths_in_a_2d_array_to(1, 1)).to eq(2)
     expect(number_of_paths_in_a_2d_array_to(2, 2)).to eq(6)
     expect(number_of_paths_in_a_2d_array_to(3, 3)).to eq(20)
     expect(number_of_paths_in_a_2d_array_to(4, 4)).to eq(70)
+  end
+
+  it "should return the correct number of paths given a coordinate pair(x,y) where x != y" do
+    expect(number_of_paths_in_a_2d_array_to(1,3)).to eq(4)
+    expect(number_of_paths_in_a_2d_array_to(1,4)).to eq(5)
   end
 end
