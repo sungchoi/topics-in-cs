06eb2687b4ea6082ced476a460d8cb987466bafd
Sung Choi <m.sung.choi@gmail.com>
2015-04-11 08:16:15 -0700

    Clean up. Functions. Ruby. Reorganizes the specs for combinations_of_ money_recursive.

--- functions/ruby/spec/combinations_of_money_recursive_spec.rb
+++ functions/ruby/spec/combinations_of_money_recursive_spec.rb
@@ -9,17 +9,9 @@ describe "combinations_of_money_recursive" do
     expect(combinations_of_money_recursive(99, [100])).to eq([])
   end
 
-  it "" do
+  it "should return an array of all possible combinations of money in the form [{denom1 => integer, denom2 => integer}, {denom1 => integer}] given a sum" do
     expect(combinations_of_money_recursive(100, [100])).to eq([{100.to_d => 1.to_d}])
-  end
-
-
-
-  it "" do
     expect(combinations_of_money_recursive(100, [1, 100])).to eq([{100.to_d => 0.to_d, 1.to_d => 100.to_d}, {100.to_d => 1.to_d, 1.to_d => 0.to_d}])
-  end
-
-  it "" do
     expect(combinations_of_money_recursive(99, [1, 100])).to eq([{100.to_d => 0.to_d, 1.to_d => 99.to_d}])
   end
 end
